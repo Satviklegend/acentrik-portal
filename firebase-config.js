502a7b72b291a3b51982a6ae870674bb9c6d8f0f
Satviklegend <168599159+Satviklegend@users.noreply.github.com>
2025-12-23 18:05:42 -0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+// Replace with your actual Firebase config
+export const firebaseConfig = {
+    apiKey: "AIzaSyYOUR_API_KEY_HERE",
+    authDomain: "acentrik-portal.firebaseapp.com",
+    projectId: "acentrik-portal",
+    storageBucket: "acentrik-portal.appspot.com",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "1:YOUR_APP_ID:web:YOUR_APP_ID"
+};
